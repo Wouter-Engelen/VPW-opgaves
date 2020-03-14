0e9a566fab23d01e27367a807a53a0063fc250a0
Wouter Engelen <wouter.engelen@student.ucll.be>
2020-03-14 10:08:25 +0100

    2018/cat4/pizza Cleaned code

--- 2018/cat4/pizza/cpp/WouterE.cpp
+++ 2018/cat4/pizza/cpp/WouterE.cpp
@@ -1,66 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-using ii = pair<int, int>;
-using vi = vector<int>;
+map<pair<int, int>, int> cache;
 
-map<ii, int> cache;
+int find_maximum(const vector<int>& pizza, int left, int right, int leftover) {
+	left = (left + pizza.size()) % pizza.size(); 
+	right = (right + pizza.size()) % pizza.size();
 
-int find_maximum(const vi& pizza, int left, int right) {
-	if ((left + 1) % pizza.size() == right) {
-		return 0;
-	}
-
-	if (cache.find({ left, right }) != cache.end()) {
-		return cache.at({ left, right });
-	}
-
-	int total = 0;
-	for (int i = right; i != (left + 1) % pizza.size(); i = (i + 1) % pizza.size()) {
-		total += pizza.at(i);
-	}
+	if (left == right) return pizza[left];
+	if (cache.find({ left, right }) != cache.end()) return cache.at({ left, right });
 	
-	int min_other = min(
-		find_maximum(pizza, (left - 1 + pizza.size()) % pizza.size(), right),
-		find_maximum(pizza, left, (right + 1) % pizza.size())
+	return cache[{left, right}] = max(
+		leftover - find_maximum(pizza, left - 1, right, leftover - pizza[left]),
+		leftover - find_maximum(pizza, left, right + 1, leftover - pizza[right])
 	);
-
-	cache[{left, right}] = total - min_other;
-	return total - min_other;
 }
 
-int solve(const vi& pizza) {
+int solve(const vector<int>& pizza) {
 	int total = 0;
 	for (int a : pizza) total += a;
 
-	int min_bob = total;
-	
+	int best = 0;
 	for (int i = 0; i < pizza.size(); i++) {
-		int max = find_maximum(pizza, (i - 1 + pizza.size()) % pizza.size(), (i + 1) % pizza.size());
-		if (max < min_bob) min_bob = max;
+		best = max(best, find_maximum(pizza, i, i + 1, total));
 	}
 
-	return total - min_bob;
+	return best;
 }
 
 int main() {
-	int n;
-	cin >> n;
+	int cases;
+	cin >> cases;
 
-	for (int i = 0; i < n; i++) {
+	for (int nr = 1; nr <= cases; nr++) {
 		int c;
 		cin >> c;
 
-		vi pizza;
-		for (int k = 0; k < c; k++) {
-			int slice;
-			cin >> slice;
-			pizza.push_back(slice);
-		}
+		vector<int> pizza(c);
+		for (int k = 0; k < c; k++) cin >> pizza[k];
 
 		cache.clear();
 		cout << solve(pizza) << endl;
